ade5d01d343496e5164cf7ab3611014806be5b50
kyosakura94 <kyosakura94@gmail.com>
2020-11-27 17:07:00 -0500

    4

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/PluginDemoProject/PluginDemoProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PLUGINDEMOPROJECT_PluginDemoProjectGameModeBase_generated_h
+#error "PluginDemoProjectGameModeBase.generated.h already included, missing '#pragma once' in PluginDemoProjectGameModeBase.h"
+#endif
+#define PLUGINDEMOPROJECT_PluginDemoProjectGameModeBase_generated_h
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_SPARSE_DATA
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_RPC_WRAPPERS
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPluginDemoProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_APluginDemoProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APluginDemoProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PluginDemoProject"), NO_API) \
+	DECLARE_SERIALIZER(APluginDemoProjectGameModeBase)
+
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAPluginDemoProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_APluginDemoProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APluginDemoProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PluginDemoProject"), NO_API) \
+	DECLARE_SERIALIZER(APluginDemoProjectGameModeBase)
+
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APluginDemoProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APluginDemoProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APluginDemoProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APluginDemoProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APluginDemoProjectGameModeBase(APluginDemoProjectGameModeBase&&); \
+	NO_API APluginDemoProjectGameModeBase(const APluginDemoProjectGameModeBase&); \
+public:
+
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APluginDemoProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APluginDemoProjectGameModeBase(APluginDemoProjectGameModeBase&&); \
+	NO_API APluginDemoProjectGameModeBase(const APluginDemoProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APluginDemoProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APluginDemoProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APluginDemoProjectGameModeBase)
+
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_12_PROLOG
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_SPARSE_DATA \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_RPC_WRAPPERS \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_INCLASS \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_SPARSE_DATA \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PLUGINDEMOPROJECT_API UClass* StaticClass<class APluginDemoProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID PluginDemoProject_Source_PluginDemoProject_PluginDemoProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
